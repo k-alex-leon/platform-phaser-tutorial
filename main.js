c48a0edddc115bc98adc4764f7e5b8c4a6f70e19
k-alex-leon <kelemon824@gmail.com>
2024-06-28 20:42:23 -0500

    feat: player instance and platform-player collision

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ const config = {
 let game = new Phaser.Game(config);
 
 let platforms;
+let player;
 
 function preload() {
   this.load.image("sky", "/sky.png");
@@ -40,6 +41,7 @@ function preload() {
 }
 
 function create() {
+  // WORLD CONFIG
   this.add.image(400, 300, "sky").setScale(2, 1);
 
   platforms = this.physics.add.staticGroup();
@@ -48,6 +50,34 @@ function create() {
   platforms.create(600, 400, "ground");
   platforms.create(50, 250, "ground");
   platforms.create(750, 220, "ground");
+
+  // PLAYER CONFIG
+  player = this.physics.add.sprite(100, 450, "dude");
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  // COLLISION OBJECTS
+  this.physics.add.collider(player, platforms);
 }
 
 function update() {}
