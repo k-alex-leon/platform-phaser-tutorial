0a5a1082d9f844b35c4414807502a463969fccfd
k-alex-leon <kelemon824@gmail.com>
2024-06-28 20:16:30 -0500

    feat: platforms added

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ import Phaser from "phaser";
 
 const config = {
   type: Phaser.AUTO,
-  with: 800,
-  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: true,
+    },
+  },
   scene: {
     preload,
     create,
@@ -13,13 +18,36 @@ const config = {
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
+    with: 800,
+    height: 600,
   },
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 
-function preload() {}
+let platforms;
 
-function create() {}
+function preload() {
+  this.load.image("sky", "/sky.png");
+  this.load.image("ground", "/platform.png");
+  this.load.image("star", "/star.png");
+  this.load.image("bomb", "/bomb.png");
+
+  this.load.spritesheet("dude", "/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+}
+
+function create() {
+  this.add.image(400, 300, "sky").setScale(2, 1);
+
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, "ground").setScale(4, 2).refreshBody();
+
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
+}
 
 function update() {}
